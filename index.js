095c9e6da02eb297eea49f888f780cac0cff12b4
tanvirhossains <tanvirhossaincou2020@gmail.com>
2022-05-24 11:06:17 +0600

    get the tools data

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.TOOLS_USER}:${process.env.TOOLS_PASSWORD}@cluster0.wdai4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    await client.connect();
+
+
+
+    const toolsCollection = client.db("construction_tools").collection("tools");
+
+    app.get('/tools', async (req, res) => {
+
+      const query = {};
+      const result = await toolsCollection.find(query).toArray();
+      res.send(result)
+    })
+  }
+  finally {
+    // await client.close();
+  }
+}
+run()
 
 
 
@@ -9,5 +46,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log('its ok', port)
 })
