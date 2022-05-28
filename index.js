386846b7f28c59683ac909c2ca534f58a30bc55b
tanvirhossains <tanvirhossaincou2020@gmail.com>
2022-05-28 23:58:14 +0600

    added

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors');
-require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { accepts } = require('express/lib/request');
 
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+
+
+
+
 
 
 
